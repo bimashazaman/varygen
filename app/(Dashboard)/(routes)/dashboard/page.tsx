7f07dc0db0c270017dcd5b93daaf96da04903f70
Jim <jimmusfique@gmail.com>
2023-08-03 18:29:36 +0600

    importing Clerk

---
+++ app/(Dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+const DashboardPage = () => {
+return (
+    // <Button variant = "destructive" size = "lg">Click Me</Button>
+    <p>Dashboard Page (protected)</p>
+) 
+}
+export default DashboardPage;
